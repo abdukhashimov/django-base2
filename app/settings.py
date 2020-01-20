22dccff136eeb45b5c4b4b0b3dc185a131b9cb42
Madiyor <abdukhashimov@yandex.ru>
2020-01-21 01:27:41 +0500

    added solution from google

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,8 @@ import os
 import datetime
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES = { 'default': dj_database_url.config() }
+# DATABASES['default'].update(prod_db)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
